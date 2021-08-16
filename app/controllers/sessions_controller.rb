80c1edb936c0485170608864d49cc92d4f736b2a
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-16 11:24:33 -0700

    CORS added to enable fetching from front end, refactored sessions handling to enable log in and log out from front end.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,13 @@ class SessionsController < ApplicationController
 
     def create
         
-        user = User.find_by(username: params[:username])
-        if user&.authenticate(params[:password])
+        user = User.find_by(username: params[:usernameInput])
+        if user&.authenticate(params[:passwordInput])
             session[:user_id] = user.id
             # session[:user_role] = user.role_type
             # session[:role_id] = user.role_id
             render json: user, status: :created
+            # byebug
         else
             render json: { error: "Invalid username or password" }, status: :unauthorized
         end
