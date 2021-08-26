cac7602cb89883ef40fe6aec4d8f9522df1974cf
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-26 11:27:53 -0700

    Separated card descriptions from advice by adding new column.

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,3 +1,3 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id, :img, :img_reversed, :suit, :suit_thumbnail, :arcana_type, :name, :value, :value_int, :meaning_up, :meaning_rev, :desc_up, :desc_rev
+  attributes :id, :img, :img_reversed, :suit, :suit_thumbnail, :arcana_type, :name, :value, :value_int, :meaning_up, :meaning_rev, :desc_up, :advice, :desc_rev
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_183248) do
+ActiveRecord::Schema.define(version: 2021_08_26_175630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_183248) do
     t.string "desc_rev"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "advice"
   end
 
   create_table "friends", force: :cascade do |t|
